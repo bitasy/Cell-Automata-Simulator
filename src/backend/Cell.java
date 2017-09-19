e0e1b4a050eea7359bcb6c61f9a4c624660f4d37
Brian Nieves <ban20@duke.edu>
2017-09-19 11:13:38 -0400

    Preliminary Grid/Cell setup

--- src/backend/Cell.java
+++ src/backend/Cell.java
@@ -1,5 +1,26 @@
 package backend;
 
+import javafx.scene.shape.Rectangle;
+
 public class Cell {
+	
+	private int state;
+	private Rectangle view;
+	private Grid myGrid;
+	
+	public Cell(int initialState, int width, int height, Grid grid) {
+		state = initialState;
+		myGrid = grid;
+		view = new Rectangle(width, height, myGrid.getColor(initialState));
+	}
+	
+	public void changeState(int newState) {
+		state = newState;
+		view.setFill(myGrid.getColor(newState));
+	}
+	
+	public int getState() {
+		return state;
+	}
 
 }
--- src/backend/Grid.java
+++ src/backend/Grid.java
@@ -1,5 +1,20 @@
 package backend;
 
+import java.util.HashMap;
+
+import javafx.scene.paint.Color;
+import xml_start.SimulationLoader;
+
 public class Grid {
 
+	private HashMap<Integer, Color> colorMap;
+	
+	public Grid(SimulationLoader simdata) {
+		
+	}
+	
+	Color getColor(int state) {
+		return colorMap.get(state);
+	}
 }
+
