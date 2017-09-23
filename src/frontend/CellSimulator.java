be1eb32e0270a21abe8b67d4bad6bfdd22709fa5
Simran <ss810@duke.edu>
2017-09-22 23:44:21 -0400

    add grid and extend pane for both classes

--- src/frontend/CellSimulator.java
+++ src/frontend/CellSimulator.java
@@ -1,5 +1,56 @@
 package frontend;
 
-public class CellSimulator {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import xml_start.Screen;
 
+public class CellSimulator extends Pane {
+
+	private int height = 4 * Screen.HEIGHT / 5;;
+	private double size;
+	private int rows;
+	private int cols;
+	private Timeline animation;
+	private int step;
+
+	public CellSimulator() {
+		super();
+		rows = 10;
+		cols = 10;
+		step = 1000;
+		this.resize(Screen.WIDTH, height);
+		createCellShapes();
+		animation = new Timeline(new KeyFrame(Duration.millis(step), e -> update()));
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.play();
+	}
+
+	private void update() {
+		System.out.println("updating!");
+	}
+
+	private void createCellShapes() {
+		size = Math.min(Screen.WIDTH / cols, height / rows);
+		double totalWidthPercent = size * cols / Screen.WIDTH;
+		double firstX = (.5 - totalWidthPercent / 2) * Screen.WIDTH;
+		double totalHeightPercent = size * rows / height;
+		double firstY = (.5 - totalHeightPercent / 2) * height;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				Rectangle cell = new Rectangle(size, size);
+				cell.setOnMouseClicked(e -> System.out.println("Cell clicked!"));
+				cell.setX(firstX + j * size);
+				cell.setY(firstY + i * size);
+				cell.setFill(Color.WHITE);
+				cell.setStroke(Color.BLACK);
+				cell.setStrokeWidth(2);
+				this.getChildren().add(cell);
+			}
+		}
+	}
 }
--- src/frontend/SimulationInterface.java
+++ src/frontend/SimulationInterface.java
@@ -1,5 +1,26 @@
 package frontend;
 
-public class SimulationInterface {
+import javafx.scene.Group;
+import javafx.scene.control.ChoiceBox;
+import xml_start.Screen;
 
+public class SimulationInterface extends Group {
+
+	private int height = Screen.HEIGHT / 5;
+	private String[] simNames;
+
+	public SimulationInterface() {
+		super();
+		this.resize(Screen.WIDTH, height);
+		simNames = new String[] {"Segregation", "Wator", "GOL", "Fire"};
+		createDropDown();
+	}
+	
+	public void createDropDown() {
+		ChoiceBox<String> simulations = new ChoiceBox<>(); 
+		simulations.getItems().addAll(simNames);
+		simulations.setValue(simNames[0]);
+//		simulation
+		this.getChildren().add(simulations);
+	}
 }
