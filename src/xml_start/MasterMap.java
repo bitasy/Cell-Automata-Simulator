3667e029f24e262487c85daded52d339b543ad8a
Paulo Flecha <pf51@duke.edu>
2017-09-23 23:07:12 -0400

    Added error reporting; done with classes and files

--- src/xml_start/MasterMap.java
+++ src/xml_start/MasterMap.java
@@ -18,7 +18,7 @@ import javafx.stage.FileChooser.ExtensionFilter;
  * Also creates Map<String, SimulationParameter>, mapping the title of the simulation to the SimulationParameter object
  * containing all the relevant parameters for that simulation
  * 
- * @author team 14
+ * @author ptflecha
  * 
  * Adapted from Main.java code from:
  * 
@@ -29,36 +29,25 @@ public class MasterMap {
 
 	public static final String[] SIMULATION_NAMES = {"GameOfLife", "Segregation", "Fire", "PredatorPrey"};
 	private Map<String, SimulationParameters> simulationMap = null;
+	private Map<String, ArrayList<String>> errorsMap = new HashMap<String, ArrayList<String>>();
 	
-	public MasterMap() throws Exception {
+	public MasterMap() {
 		
 		makeMap();
 		
 	}
 	
-	// TODO: handle exceptions somehow
-	
 	// reads in XML file and assigns SimulationParameters values
     private static SimulationParameters read(String s) throws Exception {
+    	
 		String fileName = "data/" + s + ".xml";
         File dataFile = new File(fileName);
         SimulationParameters mySimulation = null;
+        
         if (dataFile != null) {
-            try {
-            	
-            		mySimulation = new XMLParserCS("simulation").getSimulation(dataFile, s);
-            		
-            		// System.out.println("it may have worked");
-            }
-            catch (XMLException e) {
-            	// TODO: do something relevant about error;
-            	     // System.out.println("Error loading " + s);
-            	
-//                Alert a = new Alert(AlertType.ERROR);
-//                a.setContentText(e.getMessage());
-//                a.showAndWait();
-//                System.out.println("Some error here");
-            }
+        	
+        		mySimulation = new XMLParserCS("simulation").getSimulation(dataFile, s);
+        		
         }
         
         return mySimulation;
@@ -66,43 +55,44 @@ public class MasterMap {
     
     
     // creates map of all SimulationParameter instances for each simulation type
-    	private void makeMap() throws Exception {
+    	private void makeMap() {
     	
     		simulationMap = new HashMap<String, SimulationParameters>();
-    	
+    		errorsMap.clear();
+    		
     		for (String s : SIMULATION_NAMES) {
     			
-    			// TODO: add throw and catch for errors
-    			SimulationParameters currentSimulation = read(s);
-    			simulationMap.put(s, currentSimulation);
-    			
+    			SimulationParameters currentSimulation = null;
+				try {
+					currentSimulation = read(s);
+				} catch (Exception e) {
+
+					// e.printStackTrace();
+					
+		            	ArrayList<String> errorList = new ArrayList<String>();
+	        			errorList.add(e.getMessage());
+	        			errorsMap.put(s, errorList);          		
+
+	            }	
+				
+			if (currentSimulation != null) {
+					simulationMap.put(s, currentSimulation);			
+			}
     		}
     		
     		return;
-    	
     }
 
     // getter for master map
     public Map<String, SimulationParameters> getMap() {
     		return simulationMap;
     }
-
     
-    
-    public static void main (String[] args) throws Exception {
-        
-    		MasterMap myTester = new MasterMap();
-    		
-    		for (SimulationParameters simObjects : myTester.getMap().values()) {
-    			
-    			System.out.println(simObjects.getTitle());
-        		simObjects.printGrid();
-        		// simObjects.printEP();
-        		// simObjects.printCS();
-        		System.out.println();
-    				
-    		}
+    // getter for errors map
+    public Map<String, ArrayList<String>> getErrors() {
+    		return errorsMap;
     }
+    
 
 }
 
